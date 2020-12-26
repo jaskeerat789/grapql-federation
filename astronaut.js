5792f1b37e5bbe33e084e6423f90dc8f2efdcfa3
Jaskeerat Singh <jaskee789@gmail.com>
2020-12-26 19:53:01 +0530

    Add astronaut service

---
+++ astronaut.js
@@ -0,0 +1,33 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { buildFederatedSchema } = require('@apollo/federation');
+const fetch = require('node-fetch');
+
+const port = 4001;
+const apiUrl = "http://localhost:3000";
+
+const typeDefs = gql`
+    type Astronaut @key(fields: "id") {
+        id: ID!
+        name: String
+    }
+    extend type Query {
+        astronaut(id: ID!): Astronaut,
+        astronauts: [ Astronaut ]
+    }
+`;
+
+const resolvers = {
+    Astronaut: {
+        __resolveReference: (ref) => fetch(`${apiUrl}/astronauts/${ref.id}`).then(res => res.json())
+    },
+    Query: {
+        astronaut: (_, { id }) => fetch(`${apiUrl}/astronauts/${id}`).then(res => res.json()),
+        astronauts: () => fetch(`${apiUrl}/astronauts`).then(res => res.json())
+    }
+}
+
+const server = new ApolloServer({
+    schema: buildFederatedSchema([{ typeDefs, resolvers }])
+});
+
+server.listen({ port }).then(({ url }) => console.log(`Astronaut service is running at ${url}`))
--- index.js
+++ index.js
@@ -1,62 +1,19 @@
-const { ApolloServer, gql } = require('apollo-server');
-const fetch = require('node-fetch');
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
 
 const port = 4000;
-const apiUrl = "http://localhost:3000";
+const gateway = new ApolloGateway({
+    serviceList: [
+        { name: "Astronaut", url: "http://localhost:4001" }
+    ]
+})
 
-const typeDefs = gql`
-    type Astronaut {
-        id: ID!
-        name: String
-    }
-
-    type Mission {
-        id: ID!
-        designation: String
-        crew: [ Astronaut ]
-        startDate: String,
-        EndDate: String
-    }
-
-    type Query{
-        astronaut(id: ID!): Astronaut
-        astronauts:[Astronaut]
-        mission(id:ID!): Mission
-        missions: [Mission]
-    }
-`
-
-const resolvers = {
-    Query: {
-        astronaut: (_, { id }) => {
-            return fetch(`${apiUrl}/astronauts/${id}`).then(res => res.json());
-        },
-        astronauts: () => {
-            return fetch(`${apiUrl}/astronauts`).then(res => res.json());
-        },
-        mission: (_, { id }) => {
-            return fetch(`${apiUrl}/missions/${id}`).then(res => res.json());
-        },
-        missions: () => {
-            return fetch(`${apiUrl}/missions/`).then(res => res.json());
-        }
-    },
-    Mission: {
-        crew({ crew }) {
-            return crew.map(id => ({ __typename: "Astronaut", id }))
-        }
-    },
-    Astronaut: {
-        id: ({ id }) => id,
-        name: ({ id }) => fetch(`${apiUrl}/astronauts/${id}`).then(res => res.json()).then(json => { console.log(json); return json.name })
-    }
-};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    gateway,
+    subscriptions:false
 });
 
 server.listen({ port }).then(({ url }) => {
-    console.log(`server ready at ${url}`)
+    console.log(`Server is running at ${url}`)
 })
